ab5ea0e1590c22c28b719d8b57014c18b798ecde
Jasmin Trotter <34755222+JasminTrotter@users.noreply.github.com>
2021-02-20 02:47:30 -0800

    Updates

---
+++ precache-manifest.4e61706d879c367c39b920465f9cb518.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4891b29f3f98553c030fbde094d82d",
+    "url": "/new-portfolio/index.html"
+  },
+  {
+    "revision": "55507cefcc6d45f57c47",
+    "url": "/new-portfolio/static/css/main.cdf9094a.chunk.css"
+  },
+  {
+    "revision": "7b039e277f02b50bdb7d",
+    "url": "/new-portfolio/static/js/2.b21e9944.chunk.js"
+  },
+  {
+    "revision": "55507cefcc6d45f57c47",
+    "url": "/new-portfolio/static/js/main.4743207f.chunk.js"
+  },
+  {
+    "revision": "8449b915f8e9092f6f16",
+    "url": "/new-portfolio/static/js/runtime-main.3450413c.js"
+  },
+  {
+    "revision": "9101da5632701e010eb32fa43418e5fe",
+    "url": "/new-portfolio/static/media/JTROTTER_Resume_2021.9101da56.pdf"
+  },
+  {
+    "revision": "54be010803b6254b720e1124f709ceee",
+    "url": "/new-portfolio/static/media/headshot.54be0108.jpg"
+  },
+  {
+    "revision": "e3a6038a529a4742e3f4ed67577806e0",
+    "url": "/new-portfolio/static/media/instalogo.e3a6038a.png"
+  }
+]);
